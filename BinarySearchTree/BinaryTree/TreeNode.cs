7ec7265a65975d6edbfe2458dbcc292584d30254
Vlad <vladpiligrimov@gmail.com>
2020-11-26 22:19:13 +0500

    BinaryTree done

---
+++ BinarySearchTree/BinaryTree/TreeNode.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace BinarySearchTree.BinaryTree
+{
+    public class TreeNode<TKey, TValue>
+    {
+        public KeyValuePair<TKey, TValue> KeyValuePair { get; set; }
+        public TreeNode<TKey, TValue> Left, Right;
+
+        public TreeNode(KeyValuePair<TKey, TValue> keyValuePair)
+        {
+            KeyValuePair = keyValuePair;
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using BinarySearchTree.BinaryTree;
+
+namespace BinarySearchTree
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            var tree = new BinaryTree<int, int>();
+            tree.Add(5, 0);
+            tree.Add(2, 0);
+            tree.Add(1, 0);
+            tree.Add(3, 0);
+            tree.Add(7, 0);
+            tree.Add(6, 0);
+            tree.Add(8, 0);
+
+            tree.Remove(7);
+        }
+    }
+}
